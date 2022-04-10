ba0cacb13709ac8b8946a7bd798c1b60e5af5266
Rauf Alibakhshov <91628590+Alibakhshov@users.noreply.github.com>
2022-04-10 22:48:17 +0500

    this commit adds several new labels and frames to the project

--- ID card generator.py
+++ ID card generator.py
@@ -1,5 +1,6 @@
 from tkinter import *
-from PIL import Image as I 
+from PIL import Image as I
+from tkinter import ttk 
 from PIL import ImageTk as IT
 from PIL import ImageDraw as ID
 from PIL import ImageFont as IF
@@ -73,7 +74,7 @@ class main_window:
         )
         
          # creating a label for first name
-        self.first_name = Label(self.frame1,
+        self.full_name = Label(self.frame1,
                                 text = 'Full name',
                                 bg = 'white',
                                 font = 20,                    
@@ -114,26 +115,108 @@ class main_window:
                                 font = 20,                    
         )
         
+        # creating an entry for the "Full name" label
+        self.fullname_entry = Entry(self.frame1,
+                                    font = 5, 
+                                    width = 30,
+                                    bg = "white", 
+                                    fg = "black"                           
+                                    )
         
+         # creating an entry for the "gender" label
+        self.gender_entry = ttk.Combobox(self.frame1,
+                                    font = 5, 
+                                    width = 28,
+                                    state='readonly'                                                           
+                                    )
+        self.gender_entry['values'] = ('Male', 'Female', 'Others')
+        self.gender_entry.current()
+        
+        # creating an entry for the "age" label
+        self.age_entry = Entry(self.frame1,
+                                    font = 5, 
+                                    width = 30,
+                                    bg = "white", 
+                                    fg = "black"                           
+                                    )
+        
+        # creating an entry for the "blood group" label
+        self.blood_entry = Entry(self.frame1,
+                                    font = 5, 
+                                    width = 30,
+                                    bg = "white", 
+                                    fg = "black"                           
+                                    )
+        
+        # creating an entry for the "phone" label
+        self.phone_entry = Entry(self.frame1,
+                                    font = 5, 
+                                    width = 30,
+                                    bg = "white", 
+                                    fg = "black"                           
+                                    )
+        
+        # creating an entry for the "email" label
+        self.email_entry = Entry(self.frame1,
+                                    font = 5, 
+                                    width = 30,
+                                    bg = "white", 
+                                    fg = "black"                           
+                                    )
+        
+        # creating a button to generate an id card
+        self.gen_button = Button(self.frame1,
+                                 text = 'Generate',
+                                 font = ('cooper', 15, 'bold')
+                                 )
+        # creating a button to clear the data
+        self.clear_button = Button(self.frame1,
+                                 text = 'Clear',
+                                 font = ('cooper', 15, 'bold')
+                                 )
+        
+        # printing a message on the frame1
+        self.msg = 'asdfsd'
+        self.msg_frame1 = Frame(self.frame1, 
+                                bg = 'white',
+                                relief=SUNKEN, 
+                                bd=1
+                                )
+        
+        # creating a label for the frame1 message
+        self.msg_label = Label(self.msg_frame1, 
+                              text = self.msg,
+                              font = ('cooper', 15, 'bold'),
+                              fg = 'red',
+                              bg = 'white'
+                              )
         
         
         # placing the variables
         self.title.place(x=0, y=0, relwidth=1)
         self.logo.place(x=3, y=2, width=70, height=70)
         self.logo_text.place(x=0, y=0, relheight=1, relwidth=1)
-        self.frame1.place(x=10, y=100, width=665, height=550)
-        self.frame2.place(x=685, y=100, width=665, height=550)
+        self.frame1.place(x=10, y=100, width=665, height=650)
+        self.frame2.place(x=685, y=100, width=665, height=650)
         self.frame1_tit.place(x=0, y=0, relwidth=1)
         self.frame2_tit.place(x=0, y=0, relwidth=1)
-        self.first_name.place(x=10, y=80)
+        self.full_name.place(x=10, y=80)
         self.gender.place(x=10, y=140)
         self.age.place(x=10, y=200)
         self.blood_group.place(x=10, y=260)
         self.phone.place(x=10, y=320)
         self.email.place(x=10, y=380)
+        self.fullname_entry.place(x=180, y=80)
+        self.gender_entry.place(x=180, y=140)
+        self.age_entry.place(x=180, y=200)
+        self.blood_entry.place(x=180, y=260)
+        self.phone_entry.place(x=180, y=320)
+        self.email_entry.place(x=180, y=380)
+        self.gen_button.place(x=10, y=430, width=200)
+        self.clear_button.place(x=350, y=430, width=200)
+        self.msg_frame1.place(x=10,y=490, width=625, height=150)
+        self.msg_label.place(x=0, y=0, relwidth=1, relheight=1)
         
-        
-    
     
 root = Tk()
 App = main_window(root)
